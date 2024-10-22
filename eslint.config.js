f1249f6612e01e3dd16d7aee1e7f7fcfd375e198
y.ostriianskyi <ostriianskyiyurii@gmail.com>
2024-10-22 20:14:26 +0200

    ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,49 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import typescript from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
-const config = [
+export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    ignores: ["dist"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: typescriptParser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
-    },
-    settings: {
-      react: { version: "18.3" },
     },
     plugins: {
-      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "@typescript-eslint": typescript,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+      ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      "@typescript-eslint/explicit-module-boundary-types": "off",
     },
-  },
-];
-
-export default config;
+  }
+);
