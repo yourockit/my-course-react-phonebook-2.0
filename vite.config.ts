fe9c669b45bf857cd2299ede729a6ac52f839717
y.ostriianskyi <ostriianskyiyurii@gmail.com>
2024-10-22 20:02:56 +0200

    start ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
   build: {
-    sourcemap: true,
+    outDir: "build",
+    assetsDir: "assets",
+  },
+  css: {
+    modules: false,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
